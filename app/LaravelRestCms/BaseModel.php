2bcb3ae8468a5493943b5171188eb5a19e85016a
David DiMaria <ddimaria@pacificmetrics.com>
2015-12-28 16:45:15 -0700

    Uses CacheTrait and creates getTable()

--- app/LaravelRestCms/BaseModel.php
+++ app/LaravelRestCms/BaseModel.php
@@ -5,8 +5,12 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class BaseModel extends Model {
 
+	use CacheTrait;
 	use ValidatesRequests;
 
+	/**
+	 * @var \Validator
+	 */
 	protected $validation;
 
 	/**
@@ -27,6 +31,17 @@ class BaseModel extends Model {
 		$this->validation = \Validator::make($data, $rules);
 		
 		return $this->validation->passes();
+	}
+	
+	/**
+	 * Gets the table name of the model.
+	 * Override this for custom names to be used in the caching engine.
+	 * 
+	 * @return string
+	 */
+	public function getTable()
+	{
+		return $this->table;
 	}
 
 	/**
