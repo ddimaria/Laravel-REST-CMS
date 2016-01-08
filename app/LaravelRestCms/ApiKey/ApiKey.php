c77e95ef83927767ae9ac127f8d331a436bfc674
David DiMaria <ddimaria@pacificmetrics.com>
2016-01-08 09:11:15 -0700

    Updates ApiKey model to extend \Chrisbjr\ApiGuard\Models\ApiKey instead of Model

--- app/LaravelRestCms/ApiKey/ApiKey.php
+++ app/LaravelRestCms/ApiKey/ApiKey.php
@@ -3,9 +3,7 @@
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class ApiKey extends Model {
-
-	use SoftDeletes;
+class ApiKey extends \Chrisbjr\ApiGuard\Models\ApiKey {
 
 	/**
 	 * The database table used by the model.
