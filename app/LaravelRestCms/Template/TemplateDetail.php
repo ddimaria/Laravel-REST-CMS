684e5fed58f7820481550e5da15652522e4eca9a
David DiMaria <ddimaria@pacificmetrics.com>
2016-01-11 08:10:35 -0700

    Creates TemplateDetail model

---
+++ app/LaravelRestCms/Template/TemplateDetail.php
@@ -0,0 +1,43 @@
+<?php namespace App\LaravelRestCms\Template;
+
+use App\LaravelRestCms\BaseModel;
+
+class TemplateDetail extends BaseModel {
+
+	public static $searchCols = ['name', 'description', 'var'];
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'template_detail';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [];
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [];
+
+	public static $types = [
+		'wysiwyg' => 'HTML Editor',
+		'image_picker' => 'Image Picker',
+		'date_picker' => 'Date Picker',
+		'select' => 'Select',
+		'select_multiple' => 'Select Multiple',
+		'text' => 'Text',
+		'textarea' => 'Textarea',
+		'repeater' => 'Repeating Group',
+		'picker' => 'Picker',
+		'info' => 'Info Box (Backend Only)',
+	];
+
+}
