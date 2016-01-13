17af9e5a390ce89230b9c747a5e5ee7cfee4bf28
David DiMaria <ddimaria@pacificmetrics.com>
2016-01-13 08:12:14 -0700

    Adds template_detail relationship

--- app/LaravelRestCms/Page/PageDetail.php
+++ app/LaravelRestCms/Page/PageDetail.php
@@ -1,6 +1,7 @@
 <?php namespace App\LaravelRestCms\Page;
 
 use App\LaravelRestCms\BaseModel;
+use App\LaravelRestCms\Template\TemplateDetail;
 
 class PageDetail extends BaseModel {
 
@@ -26,5 +27,15 @@ class PageDetail extends BaseModel {
 	 * @var array
 	 */
 	protected $hidden = [];
+
+	/**
+	 * Joins the page_detail table
+	 * 
+	 * @return \Illuminate\Database\Eloquent\Relations\HasOne
+	 */
+	public function templateDetail()
+    {
+        return $this->hasMany(TemplateDetail::class, 'id', 'template_detail_id');
+    }
 
 }
