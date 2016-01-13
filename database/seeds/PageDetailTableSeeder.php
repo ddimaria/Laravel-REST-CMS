0297371e1bb8e59bc09c8e2a1fd6e26c03c61c1c
David DiMaria <ddimaria@pacificmetrics.com>
2016-01-13 08:11:07 -0700

    Adds additional seeds

--- database/seeds/PageDetailTableSeeder.php
+++ database/seeds/PageDetailTableSeeder.php
@@ -10,6 +10,7 @@ class PageDetailTableSeeder extends Seeder {
         DB::table('page_detail')->delete();
 
         PageDetail::create(['page_id' => 1, 'template_detail_id' => 1, 'data' => 'First page content']);
+        PageDetail::create(['page_id' => 1, 'template_detail_id' => 2, 'data' => 'First page sub-content']);
     }
 
 }
