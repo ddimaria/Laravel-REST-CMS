7ab5b80a492378f13146dd83b2c648bb4258b076
David DiMaria <ddimaria@pacificmetrics.com>
2016-01-13 05:54:45 -0700

    Creates seeder for page_detail

---
+++ database/seeds/PageDetailTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\LaravelRestCms\Page\PageDetail;
+
+class PageDetailTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('page_detail')->delete();
+
+        PageDetail::create(['page_id' => 1, 'template_detail_id' => 1, 'data' => 'First page content']);
+    }
+
+}
