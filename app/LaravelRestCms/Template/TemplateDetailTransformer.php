1d8aeb5972b38a31c76de5cf1ff3e11750102c36
David DiMaria <info@collectivesessions.com>
2016-01-19 06:27:20 -0700

    Implements HierarchyTransformerTrait and adds transform()

--- app/LaravelRestCms/Template/TemplateDetailTransformer.php
+++ app/LaravelRestCms/Template/TemplateDetailTransformer.php
@@ -2,8 +2,36 @@
 
 use App\LaravelRestCms\BaseModel;
 use App\LaravelRestCms\BaseTransformer;
+use App\LaravelRestCms\HierarchyTransformerTrait;
 
 class TemplateDetailTransformer extends BaseTransformer {
 
-	
+	use HierarchyTransformerTrait;
+
+    /**
+     * The transformer of the parent (usually itself)
+     * 
+     * @var string
+     */
+    protected $parentTransformer = self::class;
+
+    /**
+     * Transforms a TemplateDetail model
+     * 
+     * @param  \App\LaravelRestCms\BaseModel $templateDetail
+     * @return array
+     */
+    public function transform(BaseModel $templateDetail)
+    {
+        return [
+            'id' => (int) $templateDetail->id,
+            'parent_id' => (int) $templateDetail->parent_id,
+            'name' => $templateDetail->name,
+            'description' => $templateDetail->description,
+            'var' => $templateDetail->var,
+            'type' => $templateDetail->type,
+            'data' => $templateDetail->data,
+            'sort' => (int) $templateDetail->sort,
+        ];
+    }
 }
