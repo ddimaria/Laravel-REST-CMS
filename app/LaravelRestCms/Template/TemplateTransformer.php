0b9c8357b3a82f7ccca66aefff5b857a4d5a4718
David DiMaria <info@collectivesessions.com>
2016-01-19 06:28:28 -0700

    Adds transform() and includeDetail()

--- app/LaravelRestCms/Template/TemplateTransformer.php
+++ app/LaravelRestCms/Template/TemplateTransformer.php
@@ -5,5 +5,41 @@ use App\LaravelRestCms\BaseTransformer;
 
 class TemplateTransformer extends BaseTransformer {
 
-	
+	/**
+	 * List of resources possible to include
+	 *
+	 * @var array
+	 */
+	protected $availableIncludes = [
+		'detail',
+	];
+
+    /**
+     * Transforms a Page model
+     * 
+     * @param  \App\LaravelRestCms\BaseModel $page
+     * @return array
+     
+    public function transform(BaseModel $page)
+    {
+        return [
+            'id' => (int) $page->id,
+            'parent_id' => (int)$page->parent_id,
+            'template_id' => (int) $page->template_id,
+            'nav_name' => $page->nav_name,
+            'url' => $page->url,
+            'title' => $page->title,
+        ];
+    }
+*/
+    /**
+     * Include Template Detail
+     * 
+     * @param \App\LaravelRestCms\Template\Template $template
+     * @return \League\Fractal\ItemResource
+     */
+    public function includeDetail(Template $template)
+    {
+        return $this->collection($template->detail, new TemplateDetailTransformer);
+    }
 }
