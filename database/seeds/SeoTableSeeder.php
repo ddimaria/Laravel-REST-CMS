398fb9cb1db1f25ded3614e195e67c98d4466eb3
David DiMaria <info@collectivesessions.com>
2016-02-03 06:39:03 -0700

    Adds more seo DB entries

--- database/seeds/SeoTableSeeder.php
+++ database/seeds/SeoTableSeeder.php
@@ -11,6 +11,8 @@ class SeoTableSeeder extends Seeder {
         DB::table('seo')->delete();
 
         Seo::create(['title' => 'Welcome to the blog', 'keywords' => 'laravel, cms, blog', 'description' => 'Blogging about the web', 'og_title' => 'Welcome to the blog', 'og_description' => 'Blogging about the web']);
+        Seo::create(['title' => 'Laravel REST CMS', 'keywords' => 'laravel,cms,rest', 'description' => 'Laravel REST CMS, a configurale cms for any situation', 'og_title' => 'Laravel REST CMS', 'og_description' => 'Laravel REST CMS, a configurale cms for any situation']);
+        Seo::create(['title' => 'Laravel REST CMS API Docs', 'keywords' => 'laravel,cms,rest,api,docs', 'description' => 'Laravel REST CMS API Docs, documentation for public methods and attributes', 'og_title' => 'Laravel REST CMS API Docs', 'og_description' => 'Laravel REST CMS API Docs, documentation for public methods and attributes']);
     }
 
 }
