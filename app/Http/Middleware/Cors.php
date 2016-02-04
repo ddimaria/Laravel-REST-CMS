16a1ac71f5b9ffd994cf04181eecdd781e937219
David DiMaria <info@collectivesessions.com>
2016-02-04 08:09:44 -0700

    Adds Access-Control-Expose-Headers

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,8 @@ class Cors {
 		$response = $next($request);
 
 		$response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-		$response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, X-Authorization, Access-Control-Allow-Origin');
+		$response->headers->set('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Content-Type, Content-Range, Content-Disposition, Content-Description, Accept, Authorization, X-Requested-With, X-Authorization, Access-Control-Allow-Origin');
+		$response->headers->set('Access-Control-Expose-Headers', 'Access-Control-Allow-Headers, Content-Type, Content-Range, Content-Disposition, Content-Description, Accept, Authorization, X-Requested-With, X-Authorization, Access-Control-Allow-Origin');
 		$response->headers->set('Access-Control-Allow-Credentials', 'true');
 
 		return $response;
