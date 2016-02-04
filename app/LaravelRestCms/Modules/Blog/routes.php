ccfde026a539caf0827c76253a2f7fe043537813
David DiMaria <info@collectivesessions.com>
2016-02-04 08:13:04 -0700

    Adds blog/{slug} route

--- app/LaravelRestCms/Modules/Blog/routes.php
+++ app/LaravelRestCms/Modules/Blog/routes.php
@@ -3,4 +3,5 @@
 Route::group(['prefix' => 'api/v1', 'namespace' => 'Api\V1'], function()
 {
 	Route::get('blog', '\Modules\Blog\BlogController@collection');
+	Route::get('blog/{slug}', '\Modules\Blog\BlogController@showBySlug');
 });
